e9c67c1f212aa40a26407f5a90474865f252632e
dprajktaraje765@gmail.com <dprajktaraaje765@gmail.com>
2021-05-19 16:55:17 +0530

    first commit

---
+++ api/serializers.py
@@ -0,0 +1,8 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Student
+
+class StudentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model=Student
+        fields=['id','url','name','roll','city']
